6ad49e1beafbb67af78537244d850de8eebbd5d7
tingtt <ting.taku@gmail.com>
2022-03-03 17:57:18 +0900

    [change] カラーコードに`#`を含む

--- db/init.sql
+++ db/init.sql
@@ -19,7 +19,7 @@ CREATE TABLE `projects` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` bigint UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `theme_color` char(6) NOT NULL,
+  `theme_color` char(7) NOT NULL,
   `parent_id` bigint UNSIGNED DEFAULT NULL,
   `pinned` tinyint(1) NOT NULL DEFAULT '0',
   `order` tinyint UNSIGNED NOT NULL DEFAULT '0',
