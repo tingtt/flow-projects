153ceb8f07f02d138c22ecadd2ff28e58dfd5669
tingtt <ting.taku@gmail.com>
2022-07-10 18:29:58 +0900

    [add] CORS middleware

--- flags/main.go
+++ flags/main.go
@@ -4,17 +4,31 @@ import (
 	"flag"
 )
 
+type AllowOrigins []string
+
+// Implements from flag.Value
+func (i *AllowOrigins) String() string {
+	return "my string representation"
+}
+
+// Implements from flag.Value
+func (i *AllowOrigins) Set(v string) error {
+	*i = append(*i, v)
+	return nil
+}
+
 type Flags struct {
-	Port        *uint
-	LogLevel    *uint
-	GzipLevel   *uint
-	MysqlHost   *string
-	MysqlPort   *uint
-	MysqlDB     *string
-	MysqlUser   *string
-	MysqlPasswd *string
-	JwtIssuer   *string
-	JwtSecret   *string
+	Port         *uint
+	LogLevel     *uint
+	GzipLevel    *uint
+	AllowOrigins AllowOrigins
+	MysqlHost    *string
+	MysqlPort    *uint
+	MysqlDB      *string
+	MysqlUser    *string
+	MysqlPasswd  *string
+	JwtIssuer    *string
+	JwtSecret    *string
 }
 
 var flags Flags
@@ -32,6 +46,7 @@ func parse() Flags {
 		flag.Uint("port", getUintEnv("PORT", 1323), "Server port"),
 		flag.Uint("log-level", getUintEnv("LOG_LEVEL", 2), "Log level (1: 'DEBUG', 2: 'INFO', 3: 'WARN', 4: 'ERROR', 5: 'OFF', 6: 'PANIC', 7: 'FATAL'"),
 		flag.Uint("gzip-level", getUintEnv("GZIP_LEVEL", 6), "Gzip compression level"),
+		AllowOrigins{},
 		flag.String("mysql-host", getEnv("MYSQL_HOST", "db"), "MySQL host"),
 		flag.Uint("mysql-port", getUintEnv("MYSQL_PORT", 3306), "MySQL port"),
 		flag.String("mysql-database", getEnv("MYSQL_DATABASE", "flow-projects"), "MySQL database"),
@@ -40,6 +55,7 @@ func parse() Flags {
 		flag.String("jwt-issuer", getEnv("JWT_ISSUER", "flow-users"), "JWT issuer"),
 		flag.String("jwt-secret", getEnv("JWT_SECRET", ""), "JWT secret"),
 	}
+	flag.Var(&flags.AllowOrigins, "allow-origin", "CORS allow origins")
 
 	flag.Parse()
 	return flags
