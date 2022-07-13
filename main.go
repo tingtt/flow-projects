66a2f8bfb0a1f747bbdb57c64b03d88c3a8dbde9
tingtt <ting.taku@gmail.com>
2022-07-13 15:43:43 +0900

    [update] logging

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"flow-projects/mysql"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo"
@@ -26,6 +27,30 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 	return nil
 }
 
+func logFormat() string {
+	// Refer to https://github.com/tkuchiki/alp
+	var format string
+	format += "time:${time_rfc3339}\t"
+	format += "host:${remote_ip}\t"
+	format += "forwardedfor:${header:x-forwarded-for}\t"
+	format += "req:-\t"
+	format += "status:${status}\t"
+	format += "method:${method}\t"
+	format += "uri:${uri}\t"
+	format += "size:${bytes_out}\t"
+	format += "referer:${referer}\t"
+	format += "ua:${user_agent}\t"
+	format += "reqtime_ns:${latency}\t"
+	format += "cache:-\t"
+	format += "runtime:-\t"
+	format += "apptime:-\t"
+	format += "vhost:${host}\t"
+	format += "reqtime_human:${latency_human}\t"
+	format += "x-request-id:${id}\t"
+	format += "host:${host}\n"
+	return format
+}
+
 func main() {
 	// Get command line params / env variables
 	f := flags.Get()
@@ -37,10 +62,15 @@ func main() {
 	// Echo instance
 	e := echo.New()
 
+	// Log level
+	e.Logger.SetLevel(log.Lvl(*f.LogLevel))
+	e.Logger.Infof("Log level %d", *f.LogLevel)
+
 	// Gzip
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: int(*f.GzipLevel),
 	}))
+	e.Logger.Infof("Gzip enabled with level %d", *f.GzipLevel)
 
 	// CORS
 	if f.AllowOrigins != nil && len(f.AllowOrigins) != 0 {
@@ -48,13 +78,9 @@ func main() {
 			AllowOrigins: f.AllowOrigins,
 			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		}))
+		e.Logger.Info("CORS enabled")
+		e.Logger.Debugf("CORS allow origins %s", f.AllowOrigins.String())
 	}
-
-	// Log level
-	e.Logger.SetLevel(log.Lvl(*f.LogLevel))
-
-	// Validator instance
-	e.Validator = &CustomValidator{validator: validator.New()}
 
 	// JWT
 	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
@@ -65,12 +91,27 @@ func main() {
 		},
 	}))
 
+	// Logger
+	if f.LogLevel != nil && *f.LogLevel == 1 {
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: logFormat(),
+			Output: os.Stdout,
+			Skipper: func(c echo.Context) bool {
+				return c.Path() == "/-/readiness"
+			},
+		}))
+		e.Logger.Info("Access logging with `alp`(https://github.com/tkuchiki/alp) enabled")
+	}
+
+	// Validator instance
+	e.Validator = &CustomValidator{validator: validator.New()}
+
 	//
 	// Setup DB
 	//
 
 	// DB client instance
-	e.Logger.Info(mysql.SetDSNTCP(*f.MysqlUser, *f.MysqlPasswd, *f.MysqlHost, int(*f.MysqlPort), *f.MysqlDB))
+	e.Logger.Debugf("DB DSN `%s`", mysql.SetDSNTCP(*f.MysqlUser, *f.MysqlPasswd, *f.MysqlHost, int(*f.MysqlPort), *f.MysqlDB))
 
 	// Check connection
 	d, err := mysql.Open()
@@ -80,6 +121,7 @@ func main() {
 	if err = d.Ping(); err != nil {
 		e.Logger.Fatal(err)
 	}
+	e.Logger.Info("DB connection test succeeded")
 
 	//
 	// Routes
